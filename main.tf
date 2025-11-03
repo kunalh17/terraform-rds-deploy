17136a7462d49f93865db931e4fbd81069a6c42e
kunalh17 <kunalh_bhatt@yahoo.co.in>
2025-11-04 02:29:30 +0530

    RDS Instane code

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
 }
 
 # ---------------------------
@@ -25,7 +25,7 @@ provider "aws" {
 resource "aws_security_group" "oracle_rds_sg" {
   name        = "terraform-oracle-rds-sg"
   description = "Allow Oracle DB access"
-  vpc_id      = "vpc-0c7beba8bb085aa58"
+  vpc_id      = var.vpc_id
 
   ingress {
     description = "Allow Oracle access"
@@ -52,7 +52,11 @@ resource "aws_security_group" "oracle_rds_sg" {
 # ---------------------------
 resource "aws_db_subnet_group" "oracle_rds_subnet_group" {
   name       = "terraform-oracle-rds-subnet-group"
-  subnet_ids = ["subnet-0856410169a97e2f2"]
+  subnet_ids = [
+    "subnet-0856410169a97e2f2", # ap-south-1a
+    "subnet-0d127b636e6b10137", # ap-south-1b
+    "subnet-0352ee1d21f9e1daa"  # ap-south-1c
+  ]
 
   tags = {
     Name = "Terraform-Oracle-RDS-Subnet-Group"
@@ -66,11 +70,11 @@ resource "aws_db_instance" "oracle_rds" {
   identifier              = "terraform-oracle-db"
   engine                  = "oracle-se2"
   engine_version          = "19.0.0.0.ru-2024-07.rur-2024-07.r1"
-  instance_class          = "db.t3.medium"
+  instance_class          = var.db_instance_type
   allocated_storage       = 20
   storage_type            = "gp2"
-  username                = "adminuser"
-  password                = "AdminPass123!"
+  username                = var.db_username
+  password                = var.db_password
   db_subnet_group_name    = aws_db_subnet_group.oracle_rds_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.oracle_rds_sg.id]
   skip_final_snapshot     = true
@@ -79,6 +83,7 @@ resource "aws_db_instance" "oracle_rds" {
 
   tags = {
     Name = "Terraform-Oracle-RDS"
+    Environment = var.environment
   }
 }
 
